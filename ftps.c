460e334bf33f9cf6cba8c54191069da13bf2bebd
hicks576 <hicks.576@buckeyemail.osu.edu>
2018-01-18 14:25:45 -0500

    Create ftps.c

---
+++ ftps.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main() {
+  int sd, connected_sd; // socket descriptors
+  int rc; // return value from read
+  struct sockaddr_in server_address;
+  struct sockaddr_in from_address;
+  char buffer[1000];
+  int flags = 0;
+  socklen_t from_len;
+  
+  sd = socket(AF_INET, SOCK_STREAM, flags);
+ 
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(35000);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+ 
+  bind(sd, (struct sockaddr *) &server_address, sizeof(server_address));
+
+  listen(sd, 5);
+  connected_sd = accept(sd, (struct sockaddr *) &from_address, &from_len);
+  bzero(buffer, 1000);
+ 
+  printf("received the following %s\n", buffer);
+
+  return 0;
+}
