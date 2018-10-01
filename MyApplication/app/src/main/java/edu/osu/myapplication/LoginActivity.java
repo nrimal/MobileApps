c6fefeecf477688296e61a494cd0426470708ddf
hicks576 <hicks.576@osu.edu>
2018-09-30 20:38:43 -0400

    Split login activity into two fragments

--- MyApplication/app/src/main/java/edu/osu/myapplication/LoginActivity.java
+++ MyApplication/app/src/main/java/edu/osu/myapplication/LoginActivity.java
@@ -3,10 +3,13 @@ package edu.osu.myapplication;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -20,6 +23,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -53,6 +57,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
+    private static final String TAG = "Login_Activity";
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -67,7 +72,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        Configuration config = getResources().getConfiguration();
+        Log.d(TAG, "onnCreate(Bundle) called");
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+
         // Set up the login form.
         mUsernameView = findViewById(R.id.username);
         populateAutoComplete();
