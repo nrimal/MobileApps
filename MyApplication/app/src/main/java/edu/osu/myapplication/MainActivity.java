9d5b7f7f9f7da48b3268d482fff1b568e040f769
nrimal <rimal.5@osu.edu>
2018-11-03 19:05:24 -0400

    menu items added as popops

--- MyApplication/app/src/main/java/edu/osu/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/edu/osu/myapplication/MainActivity.java
@@ -1,14 +1,13 @@
 package edu.osu.myapplication;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
     private Intent currentRunning;
@@ -25,48 +24,47 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_preferences, menu);
+        getMenuInflater().inflate(R.menu.single_menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
 
-//        View menuItemView = findViewById(R.id.)
+        View menuItemView = findViewById(R.id.single_menu); // SAME ID AS MENU ID
+        PopupMenu popupMenu = new PopupMenu(this, menuItemView);
+        popupMenu.inflate(R.menu.menu_preferences);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
 
-        switch (item.getItemId()) {
-            case R.id.home_activity:
-                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
-                currentRunning = homeIntent;
-                startActivity(homeIntent);
-                return true;
-            case R.id.calender:
-                return true;
-            case R.id.closet:
-                Intent closetIntent = new Intent(MainActivity.this, ClosetActivity.class);
-                currentRunning = closetIntent;
-                startActivity(closetIntent);
-                return true;
-            case R.id.account:
-                Intent accountIntent = new Intent(MainActivity.this, PreferencesActivity.class);
-                currentRunning = accountIntent;
-                startActivity(accountIntent);
-                return true;
-            default:
-                return true;
-        }
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_activity:
+                        Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                        currentRunning = homeIntent;
+                        startActivity(homeIntent);
+                        return true;
+                    case R.id.calender:
+                        return true;
+                    case R.id.closet:
+                        Intent closetIntent = new Intent(MainActivity.this, ClosetActivity.class);
+                        currentRunning = closetIntent;
+                        startActivity(closetIntent);
+                        return true;
+                    case R.id.account:
+                        Intent accountIntent = new Intent(MainActivity.this, PreferencesActivity.class);
+                        currentRunning = accountIntent;
+                        startActivity(accountIntent);
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+        popupMenu.show();
+        return true;
+
     }
-
-//    public void showPopup(View v){
-//        PopupMenu popup = new PopupMenu(this, v);
-//
-//    }
-//
-//    @Override
-//    public boolean onMenuItemClick(MenuItem item){
-//
-//    }
     private void killRunningActivity(){
 //        currentRunning.stack
     }
