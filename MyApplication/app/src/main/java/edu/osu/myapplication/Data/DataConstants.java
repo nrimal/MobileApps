7ea87e9781df511aea699932fccf83d4a1df6b50
hicks576 <hicks.576@buckeyemail.osu.edu>
2018-11-18 16:33:20 -0500

    Suggestions based off of next event

---
+++ MyApplication/app/src/main/java/edu/osu/myapplication/Data/DataConstants.java
@@ -0,0 +1,8 @@
+package edu.osu.myapplication.Data;
+
+public class DataConstants {
+    public static final int SHIRTS = 0;
+    public static final int TSHIRTS = 1;
+    public static final int PANTS = 2;
+    public static final int SHOES = 3;
+}
--- MyApplication/app/src/main/java/edu/osu/myapplication/SuggestionFragment.java
+++ MyApplication/app/src/main/java/edu/osu/myapplication/SuggestionFragment.java
@@ -1,12 +1,9 @@
 package edu.osu.myapplication;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,7 +13,6 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -28,14 +24,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import edu.osu.myapplication.Data.Clothing;
+import edu.osu.myapplication.Data.DataConstants;
 
 
 public class SuggestionFragment extends Fragment {
@@ -57,7 +51,11 @@ public class SuggestionFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private ImageAdapter mAdapter;
 
-
+    private int temperature;
+    private int temperaturePref;
+    private String weatherType;
+    private String userDesignPref;
+    private String nextEventCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -104,6 +102,7 @@ public class SuggestionFragment extends Fragment {
         clothingDBRetreive = new ArrayList<>();
 
         rand = new Random();
+        setNextEventCategory();
         giveSuggestion();
 
         return v;
@@ -111,35 +110,40 @@ public class SuggestionFragment extends Fragment {
 
     public void giveSuggestion(){
         clothingDBRetreive.clear();
-        for(int i=0; i<3; i++){
-          getImageData(i);
 
+        if (getNextEventCategoryTop() == "Shirt") {
+            getImageData(DataConstants.SHIRTS);
         }
-        Toast val = Toast.makeText(getActivity(), "Device shook!", Toast.LENGTH_LONG);
+        else {
+            getImageData(DataConstants.TSHIRTS);
+        }
+
+        getImageData(DataConstants.PANTS);
+        getImageData(DataConstants.SHOES);
+
+        Toast val = Toast.makeText(getActivity(), "Generating new suggestion...", Toast.LENGTH_LONG);
         val.setGravity(Gravity.CENTER,0,0);
         val.show();
     }
     private void getImageData(final int num){
 
         switch (num){
-            case 0: //get Shirt
+            case DataConstants.SHIRTS: //get Shirt
                 currRef.child("Shirt").addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if(dataSnapshot.hasChildren()){
-                            int randomIndex = rand.nextInt((int)dataSnapshot.getChildrenCount());
-                            int count=0;
+                            List<Clothing> shirts = new ArrayList<>();
 
-                            for(DataSnapshot child : dataSnapshot.getChildren()){
-                                if(count==randomIndex){
-                                    clothingItem = child.getValue(Clothing.class);
-                                    clothingDBRetreive.add(clothingItem);
-                                    break;
-                                }
-                                count++;
+                            for (DataSnapshot child : dataSnapshot.getChildren()) {
+                                shirts.add(child.getValue(Clothing.class));
                             }
-                            mAdapter = new ImageAdapter(getActivity(),clothingDBRetreive);
-                            mRecyclerView.setAdapter(mAdapter);
+
+                            if (shirts.size() > 0) {
+                                clothingDBRetreive.add(shirts.get(rand.nextInt(shirts.size())));
+                                mAdapter = new ImageAdapter(getActivity(), clothingDBRetreive);
+                                mRecyclerView.setAdapter(mAdapter);
+                            }
                         }
 
                         mProgressCircle.setVisibility(View.INVISIBLE);
@@ -152,20 +156,65 @@ public class SuggestionFragment extends Fragment {
                     }
                 });
                 break;
-            case 1://get Pants
+            case DataConstants.TSHIRTS: //get T-Shirt
+                currRef.child("T_Shirt").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()){
+                            List<Clothing> top = new ArrayList<>();
+
+                            for (DataSnapshot child : dataSnapshot.getChildren()) {
+                                top.add(child.getValue(Clothing.class));
+                            }
+
+                            if (top.size() > 0) {
+                                clothingDBRetreive.add(top.get(rand.nextInt(top.size())));
+                                mAdapter = new ImageAdapter(getActivity(), clothingDBRetreive);
+                                mRecyclerView.setAdapter(mAdapter);
+                            }
+                        }
+
+                        mProgressCircle.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(getActivity(),databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                        mProgressCircle.setVisibility(View.INVISIBLE);
+                    }
+                });
+                break;
+            case DataConstants.PANTS://get Pants
                 currRef.child("Pants").addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if(dataSnapshot.hasChildren()) {
-                            int randomIndex = rand.nextInt((int) dataSnapshot.getChildrenCount());
-                            int count = 0;
+                            List<Clothing> pants = new ArrayList<>();
+                            String type;
+
+                            if (temperature > 65 - ((temperaturePref - 3) * 2)) {
+                                type = "Shorts";
+                            } else {
+                                type = "Pants";
+                            }
 
                             for (DataSnapshot child : dataSnapshot.getChildren()) {
-                                if (count == randomIndex) {
                                     clothingItem = child.getValue(Clothing.class);
-                                    clothingDBRetreive.add(clothingItem);
+                                    //if (clothingItem.SubType.contains(type)) {
+                                        pants.add(clothingItem);
+                                        break;
+                                    //}
+                            }
+
+                            int count = 0;
+                            while (count < pants.size()) {
+                                int index = rand.nextInt(pants.size());
+                                Clothing pantsSelection = pants.get(index);
+                                if (true /*pantsSelection.SubType.equals(getNextEventCategoryPants(type))*/) {
+                                    clothingDBRetreive.add(pantsSelection);
                                     break;
                                 }
+
                                 count++;
                             }
                             mAdapter = new ImageAdapter(getActivity(), clothingDBRetreive);
@@ -182,22 +231,36 @@ public class SuggestionFragment extends Fragment {
                     }
                 });
                 break;
-            case 2://get random other vals
+            case DataConstants.SHOES://get random other vals
                 currRef.child("Shoes").addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if(dataSnapshot.hasChildren()) {
-                            int randomIndex = rand.nextInt((int) dataSnapshot.getChildrenCount());
-                            int count = 0;
+                            List<Clothing> shoes = new ArrayList<>();
+                            String weather;
+
+                            if (temperature > 65 - ((temperaturePref - 3) * 2)) {
+                                weather = "warm";
+                            } else {
+                                weather = "cold";
+                            }
 
                             for (DataSnapshot child : dataSnapshot.getChildren()) {
-                                if (count == randomIndex) {
-                                    clothingItem = child.getValue(Clothing.class);
-                                    clothingDBRetreive.add(clothingItem);
+                                shoes.add(child.getValue(Clothing.class));
+                            }
+
+                            int count = 0;
+                            while (count < shoes.size()) {
+                                int index = rand.nextInt(shoes.size());
+                                Clothing shoesSelection = shoes.get(index);
+                                if (true /*shoessSelection.SubType.equals(getNextEventCategoryShoes(weather))*/) {
+                                    clothingDBRetreive.add(shoesSelection);
                                     break;
                                 }
+
                                 count++;
                             }
+
                             mAdapter = new ImageAdapter(getActivity(), clothingDBRetreive);
                             mRecyclerView.setAdapter(mAdapter);
                         }
@@ -228,5 +291,68 @@ public class SuggestionFragment extends Fragment {
         mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
         Log.d(TAG, "onResume called");
     }
+
+    public void setNextEventCategory() {
+        nextEventCategory = "Professional";
+    }
+
+    private String getNextEventCategoryPants(String type) {
+        String value;
+        switch (nextEventCategory) {
+            case "Professional":
+                value = "Dress Pants";
+                break;
+            case "Business Casual":
+            case "Romantic":
+                value = ((type.equals("Pants") ? "Chino Pants" : "Chino Shorts"));
+                break;
+            case "Athletic":
+                value = ((type.equals("Pants") ? "Athletic Pants" : "Athletic Shorts"));
+                break;
+            default:
+                value = ((type.equals("Pants") ? "Jeans" : "Chino Shorts"));
+                break;
+        }
+
+        return value;
+    }
+
+    private String getNextEventCategoryShoes(String weather) {
+        String value;
+        switch (nextEventCategory) {
+            case "Professional":
+                value = "Dress Shoes";
+                break;
+            case "Business Casual":
+            case "Romantic":
+                value = ((weather.equals("Cold") ? "Boots" : "Sandals"));
+                break;
+            case "Athletic":
+                value = "Athletic Shoes";
+                break;
+            default:
+                value = ((weather.equals("Cold") ? "Boots" : "Athletic Shoes"));
+                break;
+        }
+
+        return value;
+    }
+
+    private String getNextEventCategoryTop() {
+        String value;
+        switch (nextEventCategory) {
+            case "Professional":
+            case "Business Casual":
+            case "Romantic":
+                value = "Shirt";
+                break;
+            default:
+                value = "TShirt";
+                break;
+        }
+
+        return value;
+    }
+
 
 }
